81bf46d9131416d7385d2ed6f545c54ac84ce91b
José Fabricio <fabricio.play.2.learn@gmail.com>
2026-01-29 10:10:02 -0400

    espacio entre benefits y hero section

--- app/page.tsx
+++ app/page.tsx
@@ -99,7 +99,7 @@ function HomeContent() {
       </section>
 
       {/* Benefits */}
-      <section className="container mx-auto px-4 -mt-20 relative z-20">
+      <section className="container mx-auto px-4 -mt-24 relative z-20">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           <motion.div 
             initial={{ opacity: 0, y: 20 }}
